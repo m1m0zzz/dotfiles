7a879499f992b32efa075e6e336bbee304718888
m1m0zzz <117814895+m1m0zzz@users.noreply.github.com>
2026-09-06 20:46:30 +0900

    update: claude-auto-continue

--- dot_bash_functions.d/aliases.sh
+++ dot_bash_functions.d/aliases.sh
@@ -7,5 +7,8 @@ alias ppppd='cd ../../../..'
 alias pppppd='cd ../../../../..'
 alias ppppppd='cd ../../../../../..'
 
+# ~/script/claude-auto-continue
+alias claude='claude-auto-continue'
+
 # Windows の adb を利用する（PATH は .bashrc 側で設定）
 alias adb='adb.exe'
